8a2baa09e44e45f32873fd568f760c8d7baa4a83
Щемелёв Евгений Владимирович <ESchemelev@alfabank.ru>
2023-11-11 18:34:32 +0300

    chore(.eslintrc.js): add import eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,35 @@ module.exports = {
     extends: ['universe/native'],
     rules: {
         'react/jsx-boolean-value': ['warn', 'always'],
+        'no-restricted-imports': [
+            'error',
+            {
+                patterns: [
+                    {
+                        group: [
+                            '~/shared/*/*/**',
+                            '~/entities/*/**',
+                            '~/features/*/**',
+                            '~/widgets/*/**',
+                            '~/pages/*/**',
+                            '~/app/**',
+                        ],
+                        message:
+                            'Direct access to the internal parts of the module is prohibited',
+                    },
+                    {
+                        group: [
+                            '../**/shared',
+                            '../**/entities',
+                            '../**/features',
+                            '../**/widgets',
+                            '../**/pages',
+                            '../**/app',
+                        ],
+                        message: 'Prefer absolute imports instead of relatives',
+                    },
+                ],
+            },
+        ],
     },
 };
